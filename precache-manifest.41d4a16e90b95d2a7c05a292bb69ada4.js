5b3c489d160d74395a3dcdd85061b65316f8fcec
ralex <ralex.rdz@gmail.com>
2019-01-22 17:55:11 -0600

    Updates

---
+++ precache-manifest.41d4a16e90b95d2a7c05a292bb69ada4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b03384237304f4bd6f6",
+    "url": "/auction-demo-react/static/css/main.19a225ec.chunk.css"
+  },
+  {
+    "revision": "9b03384237304f4bd6f6",
+    "url": "/auction-demo-react/static/js/main.9b033842.chunk.js"
+  },
+  {
+    "revision": "16fb1eafaf9ed2e402d5",
+    "url": "/auction-demo-react/static/js/1.16fb1eaf.chunk.js"
+  },
+  {
+    "revision": "c1cd9030a2cc247f66f9",
+    "url": "/auction-demo-react/static/js/runtime~main.c1cd9030.js"
+  },
+  {
+    "revision": "c3675cff9f816c4c84c21a9d4dcae495",
+    "url": "/auction-demo-react/index.html"
+  }
+];
